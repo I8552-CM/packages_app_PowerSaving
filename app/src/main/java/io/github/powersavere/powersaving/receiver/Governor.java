2847c41ad72a469ba32ea5242a69d94efd4e255f
grace5921 <androidlover5842@gmail.com>
2017-01-06 20:38:41 +0530

    get info about charger
    get charger is connected or not

--- app/src/main/java/io/github/powersavere/powersaving/receiver/Governor.java
+++ app/src/main/java/io/github/powersavere/powersaving/receiver/Governor.java
@@ -5,18 +5,30 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import static io.github.powersavere.powersaving.util.PowerUtil.isConnected;
+
 /**
  * Created by sumit on 6/1/17.
  */
 
 public class Governor extends Service {
     private String Tag="Governor";
+    private boolean ChargerConnected;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(Tag,"Governor service started");
+        if(isConnected(this)==true){
+            ChargerConnected=true;
+            Log.i(Tag,"Charger is connected");
+        }
+        else {
+            ChargerConnected=false;
+            Log.i(Tag,"Charger is not connected");
+
+        }
         return START_STICKY;
     }
-    }
+}
